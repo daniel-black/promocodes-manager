cd43981a636c5c947f8c0950fd82ead9e7ecbf6b
daniel-black <drblack651@gmail.com>
2023-02-11 20:09:34 -0700

    add a new promocode is working

---
+++ prisma/migrations/20230212023105_makestatusderived/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `Promocode` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Promocode" DROP COLUMN "status";
